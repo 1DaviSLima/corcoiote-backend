d73ad7a8d325b0750317a40156d636d3ddaf1dae
1DaviSLima <davi.lima8589@gmail.com>
2026-06-23 22:29:24 -0300

    refactor:utilizar framework para refazer projeto

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-import http, { request } from "node:http";
+import express from "express";
+
+const app = express();
 
 const users = [
 	{
@@ -15,16 +17,44 @@ const users = [
 	},
 ];
 
-http
-	.createServer((request, response) => {
-		if (request.url === "/users" && request.method === 'GET') {
-			response.writeHead(200, {
-				"content-type": "application/json",
-			});
-			return;
-		}
-		response.end(JSON.stringify(users));
-		response.writeHead(404, { "content-type": "text-plain" });
-		response.end("Not Found!");
-	})
-	.listen(Number(process.env.PORT));
+app.use(express.json());
+
+app.get("/", (_request, response) => {
+	response.json({ message: "Hello!" });
+});
+
+app.get("/users", (_request, response) => {
+	response.json(users);
+});
+app.listen(Number(process.env.PORT));
+
+// import http, { request } from "node:http";
+
+// const users = [
+// 	{
+// 		name: "Cristiano Ronaldo",
+// 		status: true,
+// 	},
+// 	{
+// 		name: "Lionel Messi",
+// 		status: true,
+// 	},
+// 	{
+// 		name: "Neymar",
+// 		status: false,
+// 	},
+// ];
+
+// http
+// 	.createServer((request, response) => {
+// 		if (request.url === "/users" && request.method === 'GET') {
+// 			response.writeHead(200, {
+// 				"content-type": "application/json",
+// 			});
+// 			return;
+// 		}
+// 		response.end(JSON.stringify(users));
+// 		response.writeHead(404, { "content-type": "text-plain" });
+// 		response.end("Not Found!");
+// 	})
+// 	.listen(Number(process.env.PORT));
