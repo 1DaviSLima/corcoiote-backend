5bdcb64b085a88a47f9334e9429318afbd22be76
1DaviSLima <davi.lima8589@gmail.com>
2026-07-14 17:38:00 -0300

    feat:adicionar clientes mocados para teste

---
+++ src/moks/invoices.moks.ts
@@ -0,0 +1,32 @@
+import type { Invoices } from "./types.ts";
+
+export const invoiced: Invoices[] = [
+{
+    id: 1,
+    value: 1000,
+    invoicerId: 1,
+    status: "pendding",
+    createdAt:"25/03/2021",
+},
+{
+    id: 2,
+    value: 2000,
+    invoicerId: 2,
+    status: "pendding",
+    createdAt:"26/04/2021",
+},
+{
+    id: 3,
+    value: 3000,
+    invoicerId: 3,
+    status: "pendding",
+    createdAt:"27/05/2021",
+},
+{
+    id: 4,
+    value: 4000,
+    invoicerId: 4,
+    status: "pendding",
+    createdAt:"28/06/2021",
+},
+];
