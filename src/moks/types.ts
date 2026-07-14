7ace0e707227fcc3daa858ea0ea223e1841dbca0
1DaviSLima <davi.lima8589@gmail.com>
2026-07-14 17:38:57 -0300

    chore:estabelecer tipos para faturas

--- src/moks/types.ts
+++ src/moks/types.ts
@@ -7,3 +7,15 @@ export type Customer = {
 export type CreateCustomer = Omit<Customer, "id" | "status">;
 type CustomerWithoutId = Omit<Customer, "id">;
 export type UpdateCustomer = Partial<CustomerWithoutId>;
+
+export type Invoices = {
+    id: number;
+    value: number;
+    invoicerId: number;
+    status: "pendding" | "paid";
+    createdAt:string;
+}
+export type CreateInvoice = Pick<Invoices, "value">
+export type UpdateInvoice = Partial<
+Omit<Invoices, "id" | "invoicerId" | "createdAt">
+>;
